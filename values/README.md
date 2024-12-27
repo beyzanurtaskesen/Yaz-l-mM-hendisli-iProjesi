981e90877fed0a52b09401ca6e94f5657a71285d
Nisasedayilmaz <1030510567@erciyes.edu.tr>
2024-12-27 22:06:34 +0300

    Add files via upload

---
+++ values/README.md
@@ -0,0 +1,4 @@
+# VALUES*
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nisasedaylmz/pen/vEBJWmQ](https://codepen.io/nisasedaylmz/pen/vEBJWmQ).
+
---
+++ values/dist/script.js
@@ -0,0 +1,21 @@
+const { useState } = React;
+
+function App() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "App" }, /*#__PURE__*/
+    React.createElement("h1", { class: "title" }, "G\xFCven,\u015Eeffafl\u0131k Ve \u0130novasyon "), /*#__PURE__*/
+
+    React.createElement("div", { className: "div" }, /*#__PURE__*/
+    React.createElement("p", { class: "text" }, " G\xFCven: \u0130\u015F ortaklar\u0131m\u0131za ve m\xFC\u015Fterilerimize kar\u015F\u0131 d\xFCr\xFCst, g\xFCvenilir ve \u015Feffaf bir yakla\u015F\u0131m sergileyerek, uzun vadeli ili\u015Fkiler kurmay\u0131 ilke ediniriz."), /*#__PURE__*/
+    React.createElement("p", { class: "text" }, "\u015Eeffafl\u0131k: Ticaretin her a\u015Famas\u0131nda \u015Feffafl\u0131k sa\u011Flamak, m\xFC\u015Fteri memnuniyetini en \xFCst d\xFCzeye \xE7\u0131karmak i\xE7in \xF6nceli\u011Fimizdir."), /*#__PURE__*/
+
+    React.createElement("p", { class: "text" }, " M\xFC\u015Fteri Odakl\u0131l\u0131k: M\xFC\u015Fterilerimizin ihtiya\xE7lar\u0131n\u0131 en iyi \u015Fekilde anlamak ve onlara en uygun \xE7\xF6z\xFCmleri sunmak, i\u015F anlay\u0131\u015F\u0131m\u0131z\u0131n temelini olu\u015Fturur."), /*#__PURE__*/
+    React.createElement("p", { class: "text" }, " \u0130novasyon ve S\xFCrekli Geli\u015Fim: Ticaretin her a\u015Famas\u0131nda yenilik\xE7i \xE7\xF6z\xFCmler geli\u015Ftirmek ve s\xFCre\xE7lerimizi s\xFCrekli iyile\u015Ftirerek sekt\xF6rdeki liderli\u011Fimizi peki\u015Ftirmek bizim i\xE7in esast\u0131r.."))));
+
+
+
+
+}
+
+// React render işlemi
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
