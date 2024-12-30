f5c90d7645cf6bb9deb2b28d53a3c8bfdb595a8e
Esra Koc <eocesra@gmail.com>
2024-12-30 10:42:26 +0300

    veri tabani entegrasyonu

--- panel-giri/src/script.js
+++ panel-giri/src/script.js
@@ -4,11 +4,24 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     var errorMessage = document.getElementById('error-message');
-    if (username === 'admin' && password === '12345') {
-        
-
-        window.location.href = 'https://codepen.io/nisasedaylmz/pen/JoPoGBW'; 
-    } else {
-        errorMessage.textContent = 'Kullanıcı adı veya şifre hatalı!';
-    }
+    
+    fetch('http://localhost:3000/login', { // Node.js sunucu adresiniz
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            window.location.href = 'https://codepen.io/nisasedaylmz/pen/JoPoGBW'; 
+        } else {
+            errorMessage.textContent = data.message; // Hata mesajını göster
+        }
+    })
+    .catch((error) => {
+        console.error('Hata:', error);
+        errorMessage.textContent = 'Bir hata oluştu. Lütfen tekrar deneyin.';
+    });
 });
