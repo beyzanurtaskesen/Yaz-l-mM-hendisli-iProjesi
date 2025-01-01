a42800c37d51ff8e807d65dbe6191a63cabaa3aa
Gevher Büşra Yetişen <gbusray.2003@gmail.com>
2025-01-01 20:13:35 +0300

    Update Navbar.jsx

--- ktglobal/src/app/layout/Navbar.jsx
+++ ktglobal/src/app/layout/Navbar.jsx
@@ -4,22 +4,25 @@ import styles from "./Navbar.module.css"
 
 export default function Navbar() {
   return (
-    React.createElement("nav", null, /#_PURE_/
-    React.createElement("ul", { class: "list" }, /#_PURE_/
-    React.createElement("li", { class: "dropdown" }, " ", /#_PURE_/React.createElement("img", { id: "kt", src: "KT_navbar.png" }), " "), /#_PURE_/
-    React.createElement("li", { class: "dropdown" }, " ", /#_PURE_/React.createElement("a", { class: "navIndexs", href: "https://github.com" }, "Anasayfa")), /#_PURE_/
-    React.createElement("li", { class: "dropdown" }, " ", /#_PURE_/React.createElement("a", { class: "navIndexs", href: " " }, "Biz Kimiz"), /#_PURE_/
-    React.createElement("ul", { class: "dropdown-menu" }, /#_PURE_/
-    React.createElement("li", null, /#_PURE_/React.createElement("a", { class: "dropdown-menu-links", href: "  " }, "Vizyonumuz")), /#_PURE_/
-    React.createElement("li", null, /#_PURE_/React.createElement("a", { class: "dropdown-menu-links", href: "#" }, "Misyonumuz")), /#_PURE_/
-    React.createElement("li", null, /#_PURE_/React.createElement("a", { class: "dropdown-menu-links", href: "#" }, "Kapsam")), /#_PURE_/
-    React.createElement("li", null, /#_PURE_/React.createElement("a", { class: "dropdown-menu-links", href: "#" }, "K\xFClt\xFCr\xFCm\xFCz")), /#_PURE_/
-    React.createElement("li", null, /#_PURE_/React.createElement("a", { class: "dropdown-menu-links", href: "#" }, "Ayr\u0131cal\u0131klar\u0131m\u0131z")), /#_PURE_/
-    React.createElement("li", null, /#_PURE_/React.createElement("a", { class: "dropdown-menu-links", href: "#" }, "Hedeflerimiz")), /#_PURE_/
-    React.createElement("li", null, /#_PURE_/React.createElement("a", { class: "dropdown-menu-links", href: "#" }, "De\u011Ferlerimiz")))), /#_PURE_/
 
-    React.createElement("li", { class: "dropdown" }, " ", /#_PURE_/React.createElement("a", { class: "navIndexs", href: " " }, "\xDCr\xFCnlerimiz"), " "), /#_PURE_/
-    React.createElement("li", { class: "dropdown" }, " ", /#_PURE_/React.createElement("a", { class: "navIndexs", href: " " }, "Hizmetlerimiz"), " "), /#_PURE_/
-    React.createElement("li", { class: "KT" }, " ", /#_PURE_/React.createElement("a", { class: "navIndexs", href: " " }, "KaliTeli Ticaret")))));
+    <nav>
+      <ul class = "list">
+        <li class="dropdown"> <img id="kt" src="KT_navbar.png"></img> </li>
+        <li class="dropdown"> <a class = "navIndexs" href="https://github.com">Anasayfa</a></li>
+        <li class="dropdown"> <a class = "navIndexs" href=" ">Biz Kimiz</a>
+          <ul class="dropdown-menu">
+            <li><a class = "dropdown-menu-links" href="  ">Vizyonumuz</a></li>
+            <li><a class = "dropdown-menu-links" href="#">Misyonumuz</a></li>
+            <li><a class = "dropdown-menu-links" href="#">Kapsam</a></li>
+            <li><a class =  "dropdown-menu-links" href="#">Kültürümüz</a></li>
+            <li><a class =  "dropdown-menu-links" href="#">Ayrıcalıklarımız</a></li>
+            <li><a class =  "dropdown-menu-links" href="#">Hedeflerimiz</a></li>
+            <li><a class =  "dropdown-menu-links" href="#">Değerlerimiz</a></li>
+          </ul></li>
+        <li class="dropdown"> <a class = "navIndexs" href=" ">Ürünlerimiz</a> </li>
+        <li class="dropdown"> <a class = "navIndexs" href=" ">Hizmetlerimiz</a> </li>
+        <li class="KT"> <a class = "navIndexs" href=" ">KaliTeli Ticaret</a></li>
+      </ul>
+    </nav>
   );
 };
