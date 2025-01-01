c452368c2c969432ace4a9d5be7a9d99d86c9da8
Gevher Büşra Yetişen <gbusray.2003@gmail.com>
2025-01-01 20:55:32 +0300

    Update Navbar.jsx

--- ktglobal/src/app/layout/Navbar.jsx
+++ ktglobal/src/app/layout/Navbar.jsx
@@ -5,21 +5,22 @@ import ForexComponent from "@/app/components/forexComponent/ForexComponent"
 
 export default function Navbar() {
   return (
-
     <nav className={styles.navContainer}>
       <ul className={styles.list}>
         <li className={styles.dropdown}> <img id="kt" src="KT_navbar.png"></img> </li>
         <li className={styles.dropdown}> <a class = "navIndexs" href="http://localhost:3000/">Anasayfa</a></li>
         <li className={styles.dropdown}> <a class = "navIndexs" href=" ">Biz Kimiz</a>
+          
           <ul className={styles.dropdownMenu}>
-            <li><a href="  ">Vizyonumuz</a></li>
-            <li><a href="#">Misyonumuz</a></li>
-            <li><a href="#">Kapsam</a></li>
-            <li><a href="http://localhost:3000/culture">Kültürümüz</a></li>
-            <li><a href="#">Ayrıcalıklarımız</a></li>
-            <li><a href="#">Hedeflerimiz</a></li>
-            <li><a href="#">Değerlerimiz</a></li>
+            <li> <a className={styles.dropdownMenuLinks} href=" "> Vizyonumuz </a> </li>
+            <li> <a className={styles.dropdownMenuLinks} href="#"> Misyonumuz </a> </li>
+            <li> <a className={styles.dropdownMenuLinks} href="#"> Kapsam </a> </li>
+            <li> <a className={styles.dropdownMenuLinks} href="#"> Kültürümüz </a> </li>
+            <li> <a className={styles.dropdownMenuLinks} href="#"> Ayrıcalıklarımız </a> </li>
+            <li> <a className={styles.dropdownMenuLinks} href="#"> Hedeflerimiz </a> </li>
+            <li> <a className={styles.dropdownMenuLinks} href="#"> Değerlerimiz </a>  </li>
           </ul></li>
+        
         <li className={styles.dropdown}> <a className={styles.navIndexs} href=" ">Ürünlerimiz</a> </li>
         <li className={styles.dropdown}> <a className={styles.navIndexs} href=" ">Hizmetlerimiz</a> </li>
         <li className={styles.KT}> <a className={styles.navIndexs} href=" ">KaliTeli Ticaret</a></li>
